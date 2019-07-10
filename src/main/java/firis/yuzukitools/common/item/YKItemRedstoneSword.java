8e881b3f91d1ddada4f2b4bdff208467ce8d1114
firis <firis.games.lapismoonlight@gmail.com>
2019-07-10 21:59:26 +0900

    レッドストーンの剣機能追加
    ・レッドストーンの剣にActiveモードを追加

--- src/main/java/firis/yuzukitools/common/item/YKItemRedstoneSword.java
+++ src/main/java/firis/yuzukitools/common/item/YKItemRedstoneSword.java
@@ -1,5 +1,7 @@
 package firis.yuzukitools.common.item;
 
+import javax.annotation.Nullable;
+
 import com.google.common.collect.Multimap;
 
 import net.minecraft.block.Block;
@@ -8,11 +10,21 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class YKItemRedstoneSword extends AbstractEnergyItem {
 
@@ -27,11 +39,20 @@ public class YKItemRedstoneSword extends AbstractEnergyItem {
 	 * コンストラクタ
 	 */
 	public YKItemRedstoneSword() {
-		super(3000);
+		super(10000);
 		
 		//攻撃力
 		this.attackDamage = 3.0F + ToolMaterial.IRON.getAttackDamage();
 		this.setFull3D();
+		
+		//シールドモーション
+		this.addPropertyOverride(new ResourceLocation("sword_active"), new IItemPropertyGetter() {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+            	return YKItemRedstoneSword.isSwordActive(stack) ? 1.0F : 0.0F;
+            }
+        });
 	}
 	
 	/**
@@ -53,7 +74,7 @@ public class YKItemRedstoneSword extends AbstractEnergyItem {
 	 */
 	@Override
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		this.extractEnergy(stack, this.useEnergy);
+		extractSwordEnergy(stack, 1);
         return true;
     }
 	
@@ -62,9 +83,25 @@ public class YKItemRedstoneSword extends AbstractEnergyItem {
      */
 	@Override
     public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
-		this.extractEnergy(stack, this.useEnergy * 2);
+		this.extractSwordEnergy(stack, 2);
         return true;
     }
+	
+	/**
+	 * 
+	 * @param rate
+	 */
+	public void extractSwordEnergy(ItemStack stack, int rate) {
+		
+		if (YKItemRedstoneSword.isSwordActive(stack)) {
+			//Activeモード
+			this.extractEnergy(stack, this.useEnergy * rate * 50);
+		} else {
+			//通常モード
+			this.extractEnergy(stack, this.useEnergy * rate);
+		}
+		
+	}
 	
 	/**
 	 * くもの巣を取得可能
@@ -84,14 +121,81 @@ public class YKItemRedstoneSword extends AbstractEnergyItem {
         if (slot == EntityEquipmentSlot.MAINHAND)
         {
         	//エネルギーを保持している場合のみ攻撃力適応
-        	if(this.isEnergyStored(stack)) {
+        	if(!this.isEnergyStored(stack)) {
+        		//エネルギーなし
+        		multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 0.0D, 0));
+        		multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -3.5D, 0));
+        	} else if(YKItemRedstoneSword.isSwordActive(stack)) {
+        		//Activeモード
+	            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", (double)this.attackDamage * 4 + 4, 0));	            
+	            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", 1.0D, 0));
+	            } else {
+        		//通常モード
 	            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", (double)this.attackDamage, 0));
-        	} else {
-	            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 0.0D, 0));
+	            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.4D, 0));
         	}
-        	multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.4000000953674316D, 0));
         }
         return multimap;
     }
 
+	
+	/**
+	 * ソードモード判定
+	 * @param stack
+	 * @return
+	 */
+	public static boolean isSwordActive(ItemStack stack) {
+		
+		if (!stack.hasTagCompound() || !stack.getTagCompound().hasKey("active")) {
+    		return false;
+    	}
+    	boolean active = stack.getTagCompound().getBoolean("active");
+    	return active ? true : false;
+	}
+	
+	
+	/**
+	 * 右クリック設置時等の処理
+	 */
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		
+		if (worldIn.isRemote) return EnumActionResult.PASS;
+		
+		//右クリック時の処理
+		itemRightClick(player.getHeldItem(hand));
+		
+		return EnumActionResult.SUCCESS;
+	}
+	
+	/**
+	 * 右クリック処理
+	 */
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		
+		if (worldIn.isRemote) return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+		
+		//右クリック時の処理
+		itemRightClick(playerIn.getHeldItem(handIn));
+		
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+	}
+	
+	/**
+	 * モード切替
+	 * @param stack
+	 */
+	public void itemRightClick(ItemStack stack) {
+	
+		NBTTagCompound nbt = new NBTTagCompound();
+		if (stack.hasTagCompound()) {
+			nbt = stack.getTagCompound();
+    	}
+		
+		boolean active = !isSwordActive(stack);
+		nbt.setBoolean("active", active);
+		
+		stack.setTagCompound(nbt);
+		
+	}
 }
