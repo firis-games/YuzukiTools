a67427ece834e9daf7511c7fcc43e81c2368fb63
firis <firis.games.lapismoonlight@gmail.com>
2019-09-22 01:40:46 +0900

    インスタントハウスを新形式にあわせて変更
    ・デフォルトハウスの方向を新方式に対応
    ・チェストへのアイテム設定機能を削除

--- src/main/java/firis/yuzukitools/common/world/generator/WorldGenHouse.java
+++ src/main/java/firis/yuzukitools/common/world/generator/WorldGenHouse.java
@@ -1,17 +1,8 @@
 package firis.yuzukitools.common.world.generator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import firis.yuzukitools.YuzukiTools.YKItems;
 import firis.yuzukitools.common.instanthouse.InstantHouseManager;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
@@ -19,8 +10,6 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 
 public class WorldGenHouse extends WorldGenerator {
 
@@ -76,96 +65,7 @@ public class WorldGenHouse extends WorldGenerator {
         
         //構造体設置
         template.addBlocksToWorldChunk(world, pos, placementsettings);
-        
-        if (this.template.equals(InstantHouseManager.DEFAULT_HOUSE)) {
-	        //アイテム追加
-	        this.insertChest(position, world);
-        }
-        
-		return true;
-	}
-	
-	/**
-	 * チェストへアイテムを仕込む
-	 * @param position
-	 * @param world
-	 */
-	public void insertChest(BlockPos position, World world) {
-		
-        //チェストにアイテムを仕込む
-        BlockPos chestPos = position.up(2);
-        
-        int facing_x = 6;
-        int facing_z = 2;
-        
-        //北（標準）
-        switch (this.facing) {
-        case NORTH:
-        	chestPos = chestPos.north(facing_x).west(facing_z);
-        	break;
-        case SOUTH:
-        	chestPos = chestPos.south(facing_x).east(facing_z);
-        	break;
-        case EAST:
-        	chestPos = chestPos.north(facing_z).east(facing_x);
-        	break;
-        case WEST:
-        	chestPos = chestPos.south(facing_z).west(facing_x);
-        	break;
-       	default:
-        }
-        
-        //アイテムを追加する
-        TileEntity tile = world.getTileEntity(chestPos);
-        if (tile != null) {
-        	IItemHandler handler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-        	if (handler != null) {
-        		
-        		List<ItemStack> stackList = new ArrayList<ItemStack>();
-        		
-        		//エンチャント
-        		Enchantment efficiency = Enchantment.getEnchantmentByLocation("efficiency");
-        		Enchantment unbreaking = Enchantment.getEnchantmentByLocation("unbreaking");
-        		
-        		ItemStack toolStack;
-        		int randTool = 2;
-        		//ツール
-        		toolStack = new ItemStack(YKItems.STONE_HAMMERAXE);
-        		if (world.rand.nextInt(randTool) == 0) {
-	        		toolStack.addEnchantment(efficiency, 1);
-	        		toolStack.addEnchantment(unbreaking, 1);
-        		}
-        		stackList.add(toolStack);
-        		
-        		//防具
-        		stackList.add(new ItemStack(Items.LEATHER_HELMET));
-        		stackList.add(new ItemStack(Items.LEATHER_CHESTPLATE));
-        		stackList.add(new ItemStack(Items.LEATHER_LEGGINGS));
-        		stackList.add(new ItemStack(Items.LEATHER_BOOTS));
-        		
-        		//防具に色を設定する
-        		for (ItemStack stack : stackList) {
-        			if (stack.getItem() instanceof ItemArmor) {
-        				NBTTagCompound nbt = new NBTTagCompound();
-        				NBTTagCompound color = new NBTTagCompound();
-        				color.setInteger("color", 8991416); 
-        				//紫色
-        				nbt.setTag("display", color);
-        				stack.setTagCompound(nbt);
-        			}
-        		}
-        		
-        		//チェストへアイテムを追加する
-        		for (ItemStack stack : stackList) {
-        			for (int slot = 0; slot < handler.getSlots(); slot++) {
-                		stack = handler.insertItem(slot, stack, false);
-                		if (stack.isEmpty()) {
-                			break;
-                		}
-        			}
-        		}
-        	}
-        }
-	}
 
+        return true;
+	}
 }
