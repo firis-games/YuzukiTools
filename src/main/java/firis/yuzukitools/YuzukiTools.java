9143a79294601fdeee3211f8713a0e5b488b219c
firis <firis.games.lapismoonlight@gmail.com>
2019-09-22 21:35:55 +0900

    ver0.4

--- src/main/java/firis/yuzukitools/YuzukiTools.java
+++ src/main/java/firis/yuzukitools/YuzukiTools.java
@@ -97,7 +97,7 @@ public class YuzukiTools
 {
     public static final String MODID = "yuzukitools";
     public static final String NAME = "Yuzuki Tools";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.4";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);after:jei@[1.12.2-4.13.1.220,)";
     public static final String MOD_ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
 
